763bbd0752d72eacf0d3b5f012c36c6f87c6c948
jkilopu <jkilopu8@gmail.com>
2021-07-22 08:58:20 -0700

    Implement directions

---
+++ direction_def.h
@@ -0,0 +1,17 @@
+#ifndef _DIRECTION_DEF_H
+#define _DIRECTION_DEF_H
+
+#define DIRECTION_NUM (DOWN_RIGHT - UP + 1)
+typedef enum {
+    INVALID_DIRECTION,
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+    UP_LEFT,
+    UP_RIGHT,
+    DOWN_LEFT,
+    DOWN_RIGHT,
+} Direction;
+
+#endif
