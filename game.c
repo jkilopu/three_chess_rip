48e9440246fa98444bcfd6007a0573105a2c461e
jkilopu <jkilopu8@gmail.com>
2021-07-22 20:39:49 -0700

    Implement ask_for_choice, fix a bug

--- game.c
+++ game.c
@@ -12,6 +12,7 @@ Game *create_empty_game(unsigned int map_h, unsigned int map_w, const ChessIdx c
     Game *new_game = malloc(sizeof(Game));
     create_empty_local_round(&new_game->round, map_h, map_w, chess_nums, player_num);
     clear_settings(&new_game->settings);
+    return new_game;
 }
 
 void init_game(Game *game, const Point2D poses[], const Path paths[], unsigned int path_num, PlayerIdx start_player_idx, PlayerIdx user_player_idx)
@@ -53,38 +54,22 @@ void setup_game_from_user_input(Game *game)
     init_game(game, poses, paths, sizeof(paths) / sizeof(Path), start_player_idx, user_player_idx);
 }
 
-//bool start_round(bool robot_playing)
-//{
-//    if (!is_out(g_game.status[PLAYER_IDX(g_game.round_player)]))
-//    {
-//        while (true)
-//        {
-//            Point2D pos = {0, 0};
-//            Direction dir = ask_for_choice(g_game.round_player, &pos, robot_playing);
-//            if (move(&g_game, pos, dir))
-//                break;
-//        }
-//        player_lose(&g_game);
-//        if (!robot_playing)
-//            print_game_info();
-//    }
-//
-//    unsigned int out_cnt = 0;
-//    for (Player p = FIRST_PLAYER; p <= LAST_PLAYER; p++)
-//        out_cnt += is_out(g_game.status[PLAYER_IDX(p)]);
-//
-//    return out_cnt == PLAYER_NUM - 1;
-//}
-
-static Direction ask_for_choice(Game *game, PlayerIdx p_idx, Point2D *p_pos)
+static DirectionIdx ask_for_choice(Game *game, PlayerIdx p_idx, Point2D *p_pos)
 {
-    Direction dir; // 变量未初始化导致robot做出错误的选择？
+    DirectionIdx dir; // 变量未初始化导致robot做出错误的选择？
 
     if (p_idx == game->settings.user_player_idx)
     {
-        char input_buf[0x20];
-        read_n(input_buf, 0x20);
-        dir = parse_move_input(input_buf, p_pos);
+        MoveInfo move_info;
+        do
+        {
+            char input_buf[0x20];
+            read_n(input_buf, 0x20);
+            dir = parse_move_input(input_buf, p_pos);
+            move_info = move_try(&game->round, *p_pos, dir, false);
+            handle_move_info(move_info);
+        }
+        while (move_info != MOVE_SUCCESS);
     }
     else
     {
@@ -100,19 +85,21 @@ void game_loop(Game *game)
 {
     RoundInfo round_info;
     PlayerIdx player_num = game->round.player_array.player_num;
+    PlayerIdx player_idx = game->round.round_player_idx;
+
     do
     {
-        PlayerIdx player_idx = game->round.round_player_idx;
         if (!game->round.player_array.players[player_idx].out)
         {
             print_round(&game->round);
 
             Point2D pos;
-            Direction dir = ask_for_choice(game, player_idx, &pos);
+            DirectionIdx dir = ask_for_choice(game, player_idx, &pos);
 
             round_info = start_round(&game->round, pos, dir);
         }
         player_idx = (player_idx + 1) % player_num;
+        game->round.round_player_idx = player_idx;
     }
     while (round_info.status != GAME_END);
 }
